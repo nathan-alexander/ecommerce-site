c056037274c3ec2ff328f56e1fdb4b1333494f09
Nathan Alexander <natealexander90@gmail.com>
2023-02-13 18:58:53 -0800

    supabase

--- app/src/supabaseClient.js
+++ app/src/supabaseClient.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.DB_URL
 const supabaseKey = process.env.DB_KEY
-console.log('hello')
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
